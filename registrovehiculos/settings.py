27692df4f7072b7f652e84a02beded2cce9c5b24
DeimosCode <abraham.naranjo@alumnos.ipleones.cl>
2025-08-17 15:56:41 -0400

    pre10

--- registrovehiculos/settings.py
+++ registrovehiculos/settings.py
@@ -157,7 +157,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'vehiculos/static',  # Ruta al directorio donde están tus archivos estáticos
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
