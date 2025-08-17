148ee262996e5e68ce4aeb907c8473145b2b32b6
DeimosCode <abraham.naranjo@alumnos.ipleones.cl>
2025-08-17 15:26:36 -0400

    pre3

--- registrovehiculos/settings.py
+++ registrovehiculos/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-# SECRET_KEY = 'django-insecure-lv!s0_=6@6ju)sbs7$afmsy!gvry!896rh!669^b$u540$(-_t'
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-lv!s0_=6@6ju)sbs7$afmsy!gvry!896rh!669^b$u540$(-_t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
